336484ce2ec7dd9a8d1ab525f03852cdc2724109
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-11-20 22:09:19 +0530

    skills completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './component/Hero';
 import About from './component/About';
 import Projects from './component/Project';
 import Skills from './component/Skills';
+import Footer from './component/Footer'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -14,6 +15,7 @@ function App() {
       <About />
       <Projects />
       <Skills />
+      <Footer />
     </>
   )
 }
